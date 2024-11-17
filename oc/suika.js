d2ce1f8dcab8b1c0af744acaa4837e9dd55055d7
RLt <ltfjx2333@gmail.com>
2024-11-18 00:32:21 +0800

    ae 库存管理逻辑实现

--- oc/suika.js
+++ oc/suika.js
@@ -71,22 +71,7 @@ ws.onopen = () => {
 
     ws.send(JSON.stringify({
         type: "data/aeItemList",
-        data: {
-            uuid: "961bd7fd-89e6-47c4-a4a9-94bd52911c37",
-            itemList: [
-                {
-                    "name": "minecraft:grass",
-                    "isFluid": false,
-                    "amount": 1,
-                    "damage": 0,
-                    "craftable": false
-                }
-                // ...
-                ,
-                { "name": "water", "isFluid": true, "amount": 3000, "craftable": false },
-                { "name": "lava", "isFluid": true, "amount": 2000, "craftable": false }
-            ]
-        }
+        data: { "itemList": [{ "isFluid": false, "craftable": true, "name": "minecraft:grass", "damage": 0, "amount": 1 }, { "isFluid": false, "craftable": false, "name": "minecraft:dirt", "damage": 0, "amount": 1 }, { "isFluid": false, "craftable": false, "name": "minecraft:redstone_lamp", "damage": 0, "amount": 1 }, { "isFluid": false, "craftable": false, "name": "minecraft:bucket", "damage": 0, "amount": 5 }, { "isFluid": false, "craftable": false, "name": "minecraft:lava_bucket", "damage": 0, "amount": 1 }, { "isFluid": false, "craftable": false, "name": "minecraft:redstone", "damage": 0, "amount": 6 }, { "isFluid": false, "craftable": false, "name": "minecraft:repeater", "damage": 0, "amount": 1 }, { "isFluid": false, "craftable": false, "name": "appliedenergistics2:tile.BlockController", "damage": 0, "amount": 1 }, { "isFluid": false, "craftable": false, "name": "appliedenergistics2:tile.BlockDrive", "damage": 0, "amount": 1 }, { "isFluid": false, "craftable": false, "name": "appliedenergistics2:tile.BlockInterface", "damage": 0, "amount": 1 }, { "isFluid": false, "craftable": false, "name": "appliedenergistics2:tile.BlockAdvancedCraftingStorage", "damage": 0, "amount": 1 }, { "isFluid": false, "craftable": false, "name": "Railcraft:machine.zeta", "damage": 4, "amount": 1 }, { "isFluid": false, "craftable": false, "name": "chisel:aluminumblock", "damage": 0, "amount": 2 }, { "isFluid": false, "craftable": false, "name": "gregtech:gt.blockmachines", "damage": 15498, "amount": 1 }, { "isFluid": false, "craftable": false, "name": "OpenComputers:adapter", "damage": 0, "amount": 3 }, { "isFluid": false, "craftable": false, "name": "OpenComputers:cable", "damage": 0, "amount": 65 }, { "isFluid": false, "craftable": false, "name": "OpenComputers:case3", "damage": 0, "amount": 1 }, { "isFluid": false, "craftable": false, "name": "OpenComputers:keyboard", "damage": 0, "amount": 1 }, { "isFluid": false, "craftable": false, "name": "OpenComputers:redstone", "damage": 0, "amount": 2 }, { "isFluid": false, "craftable": false, "name": "OpenComputers:screen3", "damage": 0, "amount": 1 }, { "isFluid": false, "craftable": false, "name": "OpenComputers:caseCreative", "damage": 0, "amount": 1 }, { "isFluid": false, "craftable": false, "name": "computronics:computronics.tapeReader", "damage": 0, "amount": 1 }, { "isFluid": false, "craftable": false, "name": "appliedenergistics2:item.ItemMultiPart", "damage": 20, "amount": 65 }, { "isFluid": false, "craftable": false, "name": "appliedenergistics2:item.ItemMultiPart", "damage": 360, "amount": 1 }, { "isFluid": false, "craftable": false, "name": "ae2fc:part_fluid_terminal", "damage": 0, "amount": 1 }, { "isFluid": false, "craftable": false, "name": "OpenComputers:item", "damage": 4, "amount": 1 }, { "isFluid": false, "craftable": false, "name": "computronics:computronics.tape", "damage": 9, "amount": 1 }, { "isFluid": true, "craftable": false, "name": "water", "amount": 3000 }, { "isFluid": true, "craftable": false, "name": "lava", "amount": 2000 }], "uuid": "961bd7fd-89e6-47c4-a4a9-94bd52911c37" }
     }))
 
 }
--- public/ejs/tab/ae-view.js
+++ public/ejs/tab/ae-view.js
@@ -1,3 +1,5 @@
+var aeView__storageFilter = []
+
 document.querySelectorAll(".ae__view-back").forEach(element => {
     element.addEventListener("click", event => {
         document.getElementById("ae__view").hidden = true
@@ -7,27 +9,100 @@ document.querySelectorAll(".ae__view-back").forEach(element => {
 })
 
 globalAe.forEach(ae => {
-    let target = Array.from(document.querySelectorAll(".ae__view")).find(element => element.querySelector("data").getAttribute("uuid") === ae.uuid)
-    if (target) {
-        aeEdit__renderCpusList(target, ae)
-        aeEdit__renderItemList(target.querySelector(".ae__view-item-list"), ae)
-    }
+    aeView__storageFilter.push({
+        uuid: ae.uuid,
+        filter: {
+            type: "all", // all | item | fluid | vis
+            craftable: "all", // all | yes 只显示可合成 | no 只显示库存
+            sort: "amount", // amount | amountr | id | idr
+            word: "" // 搜索词
+        }
+    })
+
+    aeView__renderCpusList(ae.uuid, ae)
+    aeView__renderItemList(ae.uuid, ae)
+})
+
+
+document.querySelectorAll(".ae__view").forEach(aeview => {
+
+    const uuid = aeview.querySelector("data").getAttribute("uuid")
+    let filter = aeView__storageFilter.find(ae => ae.uuid == uuid).filter
+
+    aeview.querySelectorAll(".ae__view-storage-filter-button").forEach(button => {
+        button.addEventListener("click", event => {
+            filter.type = event.target.getAttribute("filter")
+            aeview.querySelectorAll(".ae__view-storage-filter-button").forEach(button => {
+                button.removeAttribute("selected")
+            })
+            event.target.setAttribute("selected", undefined)
+            aeView__renderItemList(uuid)
+        })
+    })
+
+    aeview.querySelector(".ae__view-storage-filter-craftable-button").addEventListener("click", event => {
+
+        if (filter.craftable == "all") {
+            filter.craftable = "yes"
+        } else if (filter.craftable == "yes") {
+            filter.craftable = "no"
+        } else if (filter.craftable == "no") {
+            filter.craftable = "all"
+        }
+
+        if (filter.craftable == "all") {
+            event.target.innerHTML = "库存 / 可合成"
+        } else if (filter.craftable == "yes") {
+            event.target.innerHTML = "仅可合成"
+        } else if (filter.craftable == "no") {
+            event.target.innerHTML = "仅库存"
+        }
+
+        aeView__renderItemList(uuid)
+    })
+
+    aeview.querySelector(".ae__view-storage-filter-sort-button").addEventListener("click", event => {
+
+        if (filter.sort == "amount") {
+            filter.sort = "amountr"
+        } else if (filter.sort == "amountr") {
+            filter.sort = "id"
+        } else if (filter.sort == "id") {
+            filter.sort = "idr"
+        } else if (filter.sort == "idr") {
+            filter.sort = "amount"
+        }
+
+        if (filter.sort == "amount") {
+            event.target.innerHTML = "数量排序 ↓"
+        } else if (filter.sort == "amountr") {
+            event.target.innerHTML = "数量排序 ↑"
+        } else if (filter.sort == "id") {
+            event.target.innerHTML = "ID 排序 ↓"
+        } else if (filter.sort == "idr") {
+            event.target.innerHTML = "ID 排序 ↑"
+        }
+
+        aeView__renderItemList(uuid)
+    })
+
+    aeview.querySelector(".ae__view-storage-filter-search-input").addEventListener("input", mdui.throttle(event => {
+        filter.word = event.target.value
+        aeView__renderItemList(uuid)
+    }, 100))
 })
 
 ws.addEventListener("message", async (event) => {
     const json = JSON.parse(event.data)
     if (json.type == "data/ae") {
         json.data.forEach(ae => {
-            let target = Array.from(document.querySelectorAll(".ae__view")).find(element => element.querySelector("data").getAttribute("uuid") === ae.uuid)
-            if (target) {
-                aeEdit__renderCpusList(target, ae)
-                aeEdit__renderItemList(target.querySelector(".ae__view-item-list"), ae)
-            }
+            aeView__renderCpusList(ae.uuid, ae)
+            aeView__renderItemList(ae.uuid, ae)
         })
     }
 })
 
-function aeEdit__renderCpusList(target, ae) {
+function aeView__renderCpusList(target, ae) {
     let _ = ""
 
     // if(ae)
@@ -35,20 +110,106 @@ function aeEdit__renderCpusList(target, ae) {
     // element.querySelector(".ae__view-cpus").innerHTML = _
 }
 
-function aeEdit__renderItemList(target, ae) {
+// target: 目标 ae uuid
+// ae: 目标 ae 数据，留空则从全局 ae 列表中获取
+function aeView__renderItemList(target, ae) {
     let _ = ""
 
+    let targetElement = Array.from(
+        document.querySelectorAll(".ae__view"))
+        .find(element => element.querySelector("data").getAttribute("uuid") === target)
+        .querySelector(".ae__view-item-list")
+
+    if (!ae) {
+        ae = globalAe.find(a => a.uuid === target)
+    }
+
     if (ae.itemList) {
-        ae.itemList.forEach(item => {
+        const filter = aeView__storageFilter.find(ae => ae.uuid === target).filter
+
+        itemListFiltered = ae.itemList.filter(item => {
+            if (filter.type === "all") {
+                return true
+            } else if (filter.type === "item") {
+                if (item.isFluid) {
+                    return false
+                } else {
+                    return true
+                }
+            } else if (filter.type === "fluid") {
+                if (item.isFluid) {
+                    return true
+                } else {
+                    return false
+                }
+            } else if (filter.type === "vis") {
+                // TODO: VIS 过滤实现
+                return false
+            }
+        }).filter(item => {
+            if (filter.craftable === "all") {
+                return true
+            } else if (filter.craftable === "yes") {
+                if (item.craftable) {
+                    return true
+                } else {
+                    return false
+                }
+            } else if (filter.craftable === "no") {
+                if (item.craftable) {
+                    return false
+                } else {
+                    return true
+                }
+            }
+        }).filter(item => {
+            if (filter.word === "") {
+                return true
+            } else if (filter.word[0] == "@") {
+                const modName = filter.word.slice(1)
+                if (item.name.split(":")[0].toLocaleLowerCase().includes(modName.toLocaleLowerCase())) {
+                    return true
+                } else {
+                    return false
+                }
+
+            }
+            else if (item.display.toLocaleLowerCase().includes(filter.word.toLocaleLowerCase()) || item.name.toLocaleLowerCase().includes(filter.word.toLocaleLowerCase()) || item.id == filter.word) {
+                return true
+            } else {
+                return false
+            }
+        }).sort((a, b) => {
+            if (filter.sort === "amount") {
+                return b.amount - a.amount
+            } else if (filter.sort === "amountr") {
+                return a.amount - b.amount
+            } else if (filter.sort === "id") {
+                return b.id - a.id
+            } else if (filter.sort === "idr") {
+                return a.id - b.id
+            }
+        })
+
+        itemListFiltered.forEach(item => {
+
+            let link = ""
+            let amount = item.amount
+
             if (item.isFluid) {
                 link = `fluid/${item.id}.png`
             } else {
                 link = `item/${item.id}_${item.damage}.png`
             }
+
+            if (item.craftable) {
+                amount += "(C)"
+            }
+
             _ += `
             <div style="position: relative;">
               <img src="./resources/itempanel/${link}" style="height: 3rem;"></img>
-              <div style="position: absolute;bottom: 0;right: 6px;text-align: right;">${item.amount}</div>
+              <div style="position: absolute;bottom: 0;right: 6px;text-align: right;">${amount}</div>
             </div>
             `
         })
@@ -60,5 +221,5 @@ function aeEdit__renderItemList(target, ae) {
         `
     }
 
-    target.innerHTML = _
+    targetElement.innerHTML = _
 }
