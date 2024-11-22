c887463065dd25bb903267ea8fe0b52d8ccc9b09
RLt <ltfjx2333@gmail.com>
2024-11-23 00:49:57 +0800

    完善 bot task dialog 交互

--- public/js/content/debug.js
+++ public/js/content/debug.js
@@ -16,4 +16,4 @@ document.getElementById("debug__ws-button-send").addEventListener("click", async
     ws.send(JSON.stringify({ type: "oc/forward", target: target, data: JSON.parse(data) }))
 })
 
-document.getElementById('bot__task-dialog').open = true
+document.getElementById('bot__task-dialog-step1').open = true
